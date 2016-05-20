194bbe39212100a645987faee36cb22757180399
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-05-20 00:45:41 -0400

    Fixing setup.py.

--- setup.py
+++ setup.py
@@ -3,13 +3,19 @@
 from distutils.core import setup
 
 setup(name="mne",
-      version="0.0.1",
-      description="",
+      version="0.1",
+      description="A note reminder that uses dzen2 to display notes", 
       scripts=['mne'],
       py_modules = ['libmne'],
       author="Iury O. G. Figueiredo",
       author_email="ioliveira@id.uff.br",
+      url='https://github.com/iogf/mne',
+      download_url='https://github.com/iogf/mne/releases',
+      keywords=['reminder', 'mne', 'note', 'schedule'],
       classifiers=[])
+
+
+
 
 
 
